0b985aa2d3d757f67cd3c76d31a50e384d46eb45
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-07-14 13:01:54 +0530

    added products

--- product.sql
+++ product.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2022 at 09:37 PM
+-- Generation Time: Jul 14, 2022 at 09:30 AM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,12 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `description`, `price`, `product_name`, `thumb`) VALUES
-(1, 'A makeup brush is a tool with bristles, used for the application of makeup or face painting. The bristles may be made out of natural or synthetic materials, while the handle is usually made out of plastic or wood.', '200', 'Makeup Brush ', '.\\images\\brush.png'),
+(1, 'A makeup brush is a tool with bristles, used for the application of makeup or face painting.', '200', 'Makeup Brush ', '.\\images\\brush.png'),
 (2, 'Eyeliners are products that apply color to the area around the eyes to accent and highlight appearance of the eyes', '150', 'Eyeliner', '.\\images\\eye.png'),
 (3, 'A liquid type makeup applied to the face and neck to create an even, uniform color to the complexion, cover flaws and, sometimes, to change the natural skin tone', '300', 'Foundation', '.\\images\\foundation.png'),
 (4, 'Lipstick is a cosmetic that applies color, texture, and protection to the lips. ', '50', 'Red lipstick', '.\\images\\lipstick.png'),
-(5, 'A cosmetic substance for darkening, lengthening, curling, coloring, and thickening the eyelashes, applied with a brush or rod.', '500', 'Mascara', '.\\images\\mas.png');
+(5, 'A cosmetic substance for darkening, lengthening, curling, coloring, and thickening the eyelashes, applied with a brush or rod.', '500', 'Mascara', '.\\images\\mas.png'),
+(6, 'GOES UPTO 10* SKIN LAYERS DEEP: This micro essence for the face goes deep into the skin to accelerate skin renewal and mildly exfoliates the skin.', '300', 'Moisturizer', '.\\images\\moisturizer.png');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +65,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
