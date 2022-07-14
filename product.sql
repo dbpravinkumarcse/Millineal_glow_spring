df713afb1b890890cddab3772555317d83a4a2ad
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2022-07-14 11:16:17 +0530

    added products

--- product.sql
+++ product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2022 at 03:01 PM
+-- Generation Time: Jul 13, 2022 at 09:37 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -40,11 +40,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `description`, `price`, `product_name`, `thumb`) VALUES
-(1, 'makeup brush used for face', '200', 'makeup brush', '.\\images\\brush.png'),
-(2, 'makeup eyeliner something', '150', 'eyeliner', '.\\images\\eye.png'),
-(3, 'used as face cream 120spf', '300', 'foundation', '.\\images\\foundation.png'),
-(4, 'red color stick for lips', '50', 'lipstick', '.\\images\\lipstick.png'),
-(5, 'used on eyes pour in eye for shinier eyes.', '500', 'mascara', '.\\images\\mas.png');
+(1, 'A makeup brush is a tool with bristles, used for the application of makeup or face painting. The bristles may be made out of natural or synthetic materials, while the handle is usually made out of plastic or wood.', '200', 'Makeup Brush ', '.\\images\\brush.png'),
+(2, 'Eyeliners are products that apply color to the area around the eyes to accent and highlight appearance of the eyes', '150', 'Eyeliner', '.\\images\\eye.png'),
+(3, 'A liquid type makeup applied to the face and neck to create an even, uniform color to the complexion, cover flaws and, sometimes, to change the natural skin tone', '300', 'Foundation', '.\\images\\foundation.png'),
+(4, 'Lipstick is a cosmetic that applies color, texture, and protection to the lips. ', '50', 'Red lipstick', '.\\images\\lipstick.png'),
+(5, 'A cosmetic substance for darkening, lengthening, curling, coloring, and thickening the eyelashes, applied with a brush or rod.', '500', 'Mascara', '.\\images\\mas.png');
 
 --
 -- Indexes for dumped tables
